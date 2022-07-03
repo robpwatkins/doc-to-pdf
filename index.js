cff2a0637823f8290a4382da5378b9ff4dfc30de
robpwatkins <robpwatkins@gmail.com>
2022-07-02 20:14:45 -0500

    simplifying

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/page/document', require('./controllers/document').show);
-
 app.post('/pdf/download', require('./controllers/pdf').download);
-
 app.get('/ping', (req, res) => res.send('pong'));
 
 app.listen(process.env.PORT, () => console.log(`app listening on port ${process.env.PORT}!`));
